22bb148997e08937802c42d8338fe6d8cd929c5d
Nilanth <nilanth@mallow-tech.com>
2021-12-12 21:26:21 +0530

    added loader

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,19 @@
-export default function Button({label, onClick}) {
+export default function Button({label, onClick, loader}) {
     return (
         <button type="button"
                 onClick={onClick}
-                className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500
+                className="inline-flex items-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500
                 hover:to-yellow-500 text-white font-semibold px-6 py-3 2xl:px-12 2xl:py-6 2xl:text-4xl rounded-md">
-            {label}
+            {loader &&
+            <svg className="motion-reduce:hidden animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                        stroke-width="4"/>
+                <path className="opacity-75" fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+            </svg>
+            }
+            <span>{label}</span>
         </button>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import {useSession, signIn} from "next-auth/react"
+import {useState} from "react";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
 import Followers from "../components/Followers";
@@ -6,8 +7,16 @@ import Image from "next/image";
 
 
 export default function Home() {
-    const { data: session, status } = useSession();
-    console.log(status)
+    const [loader, setLoader] = useState(false);
+    const {data: session, status} = useSession();
+
+    if (status === 'loading') return <Layout> Loading... </Layout>;
+
+    function oauthSignIn() {
+        setLoader(!loader);
+        signIn('twitter');
+    }
+
     if (session) {
         return (
             <Layout>
@@ -18,14 +27,16 @@ export default function Home() {
     return (
         <Layout>
             <div className="mb-10">
-            <Image
-                src="/cover-logo.png"
-                alt="Picture of the author"
-                width={800}
-                height={300}
-            />
+                <Image
+                    src="/cover-logo.png"
+                    alt="Picture of the author"
+                    width={800}
+                    height={300}
+                />
             </div>
-            <Button label="Login with Twitter" onClick={() => signIn('twitter')}>Sign in</Button>
+            <Button label="Login with Twitter" onClick={() => oauthSignIn()} loader={loader}>
+                Sign in
+            </Button>
         </Layout>
     )
 }
