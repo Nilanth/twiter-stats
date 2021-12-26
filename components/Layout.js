39c55802e5c572505b4cd3f634e62ef50ed0a7b8
Nilanth <nilanth@mallow-tech.com>
2021-12-26 13:34:26 +0530

    added g id to env

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout(props) {
             <Header/>
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             <Script
-                src="https://www.googletagmanager.com/gtag/js?id=40D3DQ5Y7W"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
                 strategy="afterInteractive"
             />
             <Script id="google-analytics" strategy="afterInteractive">
@@ -17,7 +17,7 @@ export default function Layout(props) {
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                        gtag('config', 'G-40D3DQ5Y7W', {
+                        gtag('config', 'G-${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {
                           page_path: window.location.pathname,
                         });
         `}
