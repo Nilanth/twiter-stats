f70ef46c4a6d4f1156c07f15ca38512db8adc840
Nilanth <nilanth@mallow-tech.com>
2021-12-26 13:31:06 +0530

    remove BMC

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,6 @@ export default function Header() {
                 <link rel="icon" href="/ts-logo.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta name="description" content="TwiterStats - Your Twitter Followers Tracker"/>
-                <script
-                    async
-                    data-name="BMC-Widget" data-cfasync="false"
-                    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="nilanth"
-                    data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF"
-                    data-position="Right" data-x_margin="18" data-y_margin="18">
-                </script>
             </Head>
     )
 }
